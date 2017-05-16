b9308872af736c7280d31a435b08a3646d01cba6
ryonext <ryonext.s@gmail.com>
2017-05-16 10:43:12 +0900

    Add simple code.

--- SamplePushNotification/ViewController.swift
+++ SamplePushNotification/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func ButtonTouchUpInside(_ sender: Any) {
+        print("OK")
+    }
 }
 
