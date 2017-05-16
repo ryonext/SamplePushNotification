7a574ff6d4497e77c9410d82e659e5c9e507c695
ryonext <ryonext.s@gmail.com>
2017-05-16 10:22:40 +0900

    Initial Commit

---
+++ SamplePushNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SamplePushNotification
+//
+//  Created by ryonext on 2017/05/16.
+//  Copyright © 2017年 ryonext. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
